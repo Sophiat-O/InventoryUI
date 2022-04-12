3141395a54e23d117f5036370268e4c039158690
Sophiat-O <lilsylph@gmail.com>
2022-04-12 13:27:06 +0200

    dashboard

--- startbootstrap-sb-admin-2-gh-pages/js/demo/chart-pie-demo.js
+++ startbootstrap-sb-admin-2-gh-pages/js/demo/chart-pie-demo.js
@@ -2,15 +2,48 @@
 Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 Chart.defaults.global.defaultFontColor = '#858796';
 
+const productLabel = [];
+const percentValue = [];
+
+    Promise.all([
+            fetch('http://localhost:8080/topproduct')
+        ]).then(function (responses) {
+            // Get a JSON object from each of the responses
+            return Promise.all(responses.map(function (response) {
+                return response.json();
+            }));
+        }).then(function (data) {
+            // Log the data to the console
+            for (var i = 0; i < data.length; i++) {
+                        var obj = data[i]
+                        for(var j = 0; j < obj.length; j++ ){
+
+                            var dataObj = obj[j];
+                            //let monthName = obj.month;
+                            productLabel.push(dataObj.product);
+                            percentValue.push(dataObj.percentage);
+                            console.log(productLabel)
+                            //console.log(obj.month);
+                         }
+
+                    }
+                    console.log(productLabel)
+        }).catch(function (error) {
+            // if there's an error, log it
+            console.log(error);
+        });
+    // Function to define innerHTML for HTML div
+
+
 // Pie Chart Example
 var ctx = document.getElementById("myPieChart");
 var myPieChart = new Chart(ctx, {
   type: 'pie',
   data: {
-    labels: ["Direct", "Referral", "Social"],
+    labels: productLabel,
     datasets: [{
-      data: [55, 30, 15],
-      backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
+      data: percentValue,
+      backgroundColor:  ['#4e73df', '#1cc88a', '#36b9cc'],
       hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
       hoverBorderColor: "rgba(234, 236, 244, 1)",
     }],
@@ -28,9 +61,9 @@ var myPieChart = new Chart(ctx, {
       caretPadding: 10,
     },
     legend: {
-      display: false
+      display: true
     },
-    cutoutPercentage: 80,
+    //cutoutPercentage: 80,
   },
 });
 function updateGraph(chart) {
